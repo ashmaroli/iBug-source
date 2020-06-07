216e1c482c852c2cbc8f92423dc4d46b55a0e3ac
Ashwin Maroli <ashmaroli@gmail.com>
2020-06-07 20:30:28 +0530

    Monkey-patch conditionally

--- _plugins/page-drop.rb
+++ _plugins/page-drop.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+return if (Jekyll::Drops.const_get("PageDrop", false) rescue false)
+
 module Jekyll
   class Page
     # For backwards-compatibility in subclasses that do not redefine
